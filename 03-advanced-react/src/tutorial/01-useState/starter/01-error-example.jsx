5f792980436ac42854b6fe52ed34dbbfa56ae2d9
Ashvin Jaiswal <ashvin.edu@gmail.com>
2025-02-28 10:50:21 +0000

    Why need useState

--- 03-advanced-react/src/tutorial/01-useState/starter/01-error-example.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/01-error-example.jsx
@@ -1,5 +1,19 @@
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let count = 0;
+  const handleClick = () => {
+    count++;
+    console.log("click button", count);
+  };
+
+  return (
+    <>
+      <h2>useState error example</h2>
+      <button className="btn" onClick={handleClick}>
+        Increase count
+      </button>
+      <p>{count}</p>
+    </>
+  );
 };
 
 export default ErrorExample;
